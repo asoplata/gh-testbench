cc403e1397533ddae6df3e3c0d605eccde68102f
Austin E. Soplata <me@asoplata.com>
2025-05-12 16:47:29 -0400

    BREAKING changes for linux, and linux artifact

--- conda-packaging/02-build-pkg.sh
+++ conda-packaging/02-build-pkg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 
 echo $'\n--> Preparing to build the actual hnn-core conda package...'
-conda-build hnn-core -c defaults -c conda-forge
+# conda-build hnn-core -c defaults -c conda-forge
+# conda-build --croot=/tmp/asdf hnn-core -c defaults -c conda-forge
+conda-build --prefix-length=80 hnn-core -c defaults -c conda-forge
 # conda-build hnn-core -c conda-forge # leads to a certain clobber
