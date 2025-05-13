6cf0eda07ecc8f38b87c2433299d92975f8de4ba
Austin E. Soplata <me@asoplata.com>
2025-05-13 13:18:15 -0400

    very close

--- conda-packaging/02-build-pkg.sh
+++ conda-packaging/02-build-pkg.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -l
 
 echo $'\n--> Preparing to build the actual hnn-core conda package...'
-# conda-build hnn-core -c defaults -c conda-forge
+if [ `uname` == Darwin ]; then
+    conda-build hnn-core -c defaults -c conda-forge
+elif [ `uname` == Linux ]; then
+    conda-build --prefix-length=80 hnn-core -c defaults -c conda-forge
+fi
+
 # conda-build --croot=/tmp/asdf hnn-core -c defaults -c conda-forge
-conda-build --prefix-length=80 hnn-core -c defaults -c conda-forge
+# conda-build --prefix-length=80 hnn-core -c defaults -c conda-forge
+
 # conda-build hnn-core -c conda-forge # leads to a certain clobber
--- conda-packaging/hnn-core/build.sh
+++ conda-packaging/hnn-core/build.sh
@@ -11,12 +11,24 @@ if [ `uname` == Darwin ]; then
     elif [ `uname -m` == x86_64 ]; then
         NRN_WHL_URL=https://files.pythonhosted.org/packages/d7/52/50ae4bf3dcc87cf71daa559f1b188b4a8e85f7f19073d976eb12821f8692/NEURON-8.2.6-cp312-cp312-macosx_10_15_x86_64.whl
     fi
+    # Adapted from https://docs.conda.io/projects/conda-build/en/latest/resources/activate-scripts.html
+    for CHANGE in "activate" "deactivate"
+    do
+        mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+        cp "${RECIPE_DIR}/env_scripts/${CHANGE}_osx.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    done
 fi
 
 if [ `uname` == Linux ]; then
     if [ `uname -m` == x86_64 ]; then
         NRN_WHL_URL=https://files.pythonhosted.org/packages/14/4e/e0c65911a59b646274ba4f6740e8705ff29863879b0a629e92666d682ebd/NEURON-8.2.6-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
     fi
+    # Adapted from https://docs.conda.io/projects/conda-build/en/latest/resources/activate-scripts.html
+    for CHANGE in "activate" "deactivate"
+    do
+        mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+        cp "${RECIPE_DIR}/env_scripts/${CHANGE}_linux.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    done
 fi
 
 ${PYTHON} -m pip install --no-deps ${NRN_WHL_URL}
