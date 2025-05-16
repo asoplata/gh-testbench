dc68a89612d2aae714a409e69df2856cd8fcf697
Austin E. Soplata <me@asoplata.com>
2025-05-16 19:07:30 -0400

    move packaging to its own repo

--- conda-packaging/hnn-core-all/recipe/run_test.py
+++
@@ -1,6 +0,0 @@
-from hnn_core import jones_2009_model, MPIBackend, simulate_dipole
-
-simulate_dipole(jones_2009_model(), tstop=20)
-
-with MPIBackend(n_procs=2):
-    simulate_dipole(jones_2009_model(), tstop=20)
--- conda-packaging/hnn-core/recipe/run_test.py
+++
@@ -1,3 +0,0 @@
-from hnn_core import jones_2009_model, simulate_dipole
-
-simulate_dipole(jones_2009_model(), tstop=20)
