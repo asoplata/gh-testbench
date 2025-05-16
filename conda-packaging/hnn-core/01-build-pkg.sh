6aa977e7cbdeb0be029f33a7993b6444428b4cf9
Austin E. Soplata <me@asoplata.com>
2025-05-16 13:20:13 -0400

    Change and document croot linux usage

--- conda-packaging/hnn-core/01-build-pkg.sh
+++ conda-packaging/hnn-core/01-build-pkg.sh
@@ -7,7 +7,39 @@ echo $'\n--> Attempting to build the minimal "hnn-core" conda package...'
 if [ `uname` == Darwin ]; then
     conda-build recipe -c defaults -c conda-forge
 elif [ `uname` == Linux ]; then
-    conda-build --prefix-length=80 recipe -c defaults -c conda-forge
-    # TODO
-    # conda-build --croot=/tmp/asdf recipe -c defaults -c conda-forge
+    conda-build --croot=/tmp/asdf recipe -c defaults -c conda-forge
+    # If the above 'croot' attempt runs into errors, especially with paths or
+    # filesystem issues, then you can try uncommenting the following command
+    # and using it instead. This 'croot' method for building on systems where
+    # the main partition is encrypted is preferred, according to conda-build's
+    # documentation, but the 'prefix-length' argument will work too. However,
+    # the documentation claims it is less portable.
+    #
+    # conda-build --prefix-length=80 recipe -c defaults -c conda-forge
+
+    echo "
+-->Post-build Linux instructions: Note that the package, if it was built
+correctly, has been built inside your TEMPORARY directory, i.e. '/tmp/asdf'.
+This is done because Conda tries to build packages in environments where the
+path exceeds 255 characters on purpose, but this is broken if you are building
+on a partition that is disk-encrypted. There are two methods documented in the
+build script to get around this, and the default build script uses one of them.
+This also means you need to do a couple extra steps:
+
+--> AFTER you have built BOTH the 'hnn-core' and 'hnn-core-all' packages, you
+should do the following:
+
+1. Copy the new directory at '/tmp/asdf/linux-64' (itself, not just the
+contents) into '\$CONDA_PREFIX/conda-bld'. For example:
+
+    cp -r /tmp/asdf/linux-64 \$CONDA_PREFIX/conda-bld
+
+2. Re-index the conda-building directory you just copied the packages into (you
+may have to 'conda install conda-index'). For example:
+
+    conda index \$CONDA_PREFIX/conda-bld
+
+3. Then, you should be good to go. Proceed with testing the packages, then
+uploading them.
+    "
 fi
